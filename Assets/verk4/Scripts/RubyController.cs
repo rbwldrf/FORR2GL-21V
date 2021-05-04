7a710d08059e370bc2e25c143a87bc979367d632
Árbjartur Kristjánsson <53384860+rbwldrf@users.noreply.github.com>
2021-05-04 16:54:54 +0000

    a

--- Assets/verk4/Scripts/RubyController.cs
+++ Assets/verk4/Scripts/RubyController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
@@ -18,6 +18,9 @@ public class RubyController : MonoBehaviour
 
     Rigidbody2D rb; Vector2 move;
     Vector2 lookDirection = new Vector2(1, 0);
+    public GameObject projectilePrefab;
+
+    public Image hpBar;
 
     // Start is called before the first frame update
     void Awake()
@@ -52,6 +55,13 @@ public class RubyController : MonoBehaviour
                 isInvincible = false;
             }
         }
+        
+        if (Input.GetButtonDown("Fire"))
+        {
+            Launch();
+        }
+
+        hpBar.fillAmount = Mathf.Lerp(hpBar.fillAmount, (float)currentHealth / (float)maxHealth,.15f);
     }
 
     void FixedUpdate()
@@ -59,6 +69,16 @@ public class RubyController : MonoBehaviour
         Vector2 position = transform.position;
         position += speed * move * Time.deltaTime;
         rb.MovePosition(position);
+    }
+
+    public void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rb.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        //anim.SetTrigger("Launch");
     }
 
     public void ChangeHealth(int amount)
